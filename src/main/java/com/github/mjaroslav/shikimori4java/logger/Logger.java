9716d747fb6ca74052451575b76b0f6d15bd9d08
Yaroslav Novitsky <rcr3211@gmail.com>
2022-01-28 06:23:04 +0300

    Reworked default logger

--- src/main/java/com/github/mjaroslav/shikimori4java/logger/Logger.java
+++ src/main/java/com/github/mjaroslav/shikimori4java/logger/Logger.java
@@ -4,19 +4,19 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public interface Logger {
-    void info(@Nullable String message, @NotNull Object... args);
+    void info(@Nullable Object message, @NotNull Object... args);
 
-    void info(@Nullable String message, @NotNull Throwable t, @NotNull Object... args);
+    void info(@Nullable Object message, @NotNull Throwable t, @NotNull Object... args);
 
-    void error(@Nullable String message, @NotNull Object... args);
+    void error(@Nullable Object message, @NotNull Object... args);
 
-    void error(@Nullable String message, @NotNull Throwable t, @NotNull Object... args);
+    void error(@Nullable Object message, @NotNull Throwable t, @NotNull Object... args);
 
-    void warn(@Nullable String message, @NotNull Object... args);
+    void warn(@Nullable Object message, @NotNull Object... args);
 
-    void warn(@Nullable String message, @NotNull Throwable t, @NotNull Object... args);
+    void warn(@Nullable Object message, @NotNull Throwable t, @NotNull Object... args);
 
-    void debug(@Nullable String message, @NotNull Object... args);
+    void debug(@Nullable Object message, @NotNull Object... args);
 
-    void debug(@Nullable String message, @NotNull Throwable t, @NotNull Object... args);
+    void debug(@Nullable Object message, @NotNull Throwable t, @NotNull Object... args);
 }
--- src/main/java/com/github/mjaroslav/shikimori4java/logger/impl/DefaultLogger.java
+++ src/main/java/com/github/mjaroslav/shikimori4java/logger/impl/DefaultLogger.java
@@ -8,56 +8,64 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DefaultLogger implements Logger {
+    private static final Level LEVEL = Level.valueOf(System.getProperty("s4j.logger.level", "INFO").toUpperCase());
+
     private static final SimpleDateFormat FORMAT = new SimpleDateFormat("y-MM-dd HH:mm:ss.SSS");
 
     @Override
-    public void info(@Nullable String message, @NotNull Object... args) {
-        log("INFO", message, null, args);
+    public void info(@Nullable Object message, @NotNull Object... args) {
+        log(Level.INFO, message, null, args);
     }
 
     @Override
-    public void info(@Nullable String message, @NotNull Throwable t, @NotNull Object... args) {
-        log("INFO", message, t, args);
+    public void info(@Nullable Object message, @NotNull Throwable t, @NotNull Object... args) {
+        log(Level.INFO, message, t, args);
     }
 
     @Override
-    public void error(@Nullable String message, @NotNull Object... args) {
-        log("ERROR", message, null, args);
+    public void error(@Nullable Object message, @NotNull Object... args) {
+        log(Level.ERROR, message, null, args);
     }
 
     @Override
-    public void error(@Nullable String message, @NotNull Throwable t, @NotNull Object... args) {
-        log("ERROR", message, t, args);
+    public void error(@Nullable Object message, @NotNull Throwable t, @NotNull Object... args) {
+        log(Level.ERROR, message, t, args);
     }
 
     @Override
-    public void warn(@Nullable String message, @NotNull Object... args) {
-        log("WARNING", message, null, args);
+    public void warn(@Nullable Object message, @NotNull Object... args) {
+        log(Level.WARN, message, null, args);
     }
 
     @Override
-    public void warn(@Nullable String message, @NotNull Throwable t, @NotNull Object... args) {
-        log("WARNING", message, t, args);
+    public void warn(@Nullable Object message, @NotNull Throwable t, @NotNull Object... args) {
+        log(Level.WARN, message, t, args);
     }
 
     @Override
-    public void debug(@Nullable String message, @NotNull Object... args) {
-        log("DEBUG", message, null, args);
+    public void debug(@Nullable Object message, @NotNull Object... args) {
+        log(Level.DEBUG, message, null, args);
     }
 
     @Override
-    public void debug(@Nullable String message, @NotNull Throwable t, @NotNull Object... args) {
-        log("DEBUG", message, t, args);
+    public void debug(@Nullable Object message, @NotNull Throwable t, @NotNull Object... args) {
+        log(Level.DEBUG, message, t, args);
     }
 
-    private static void log(@NotNull String level, @Nullable String message, @Nullable Throwable t,
+    private static void log(@NotNull Level level, @Nullable Object message, @Nullable Throwable t,
                             @NotNull Object... args) {
+        if (level.ordinal() < LEVEL.ordinal())
+            return;
         if (message == null)
             message = "";
-        message = String.format(message, args);
-        System.out.printf("%s %s [%s] - %s%n", FORMAT.format(new Date()), level, Thread.currentThread().getName(),
+        message = String.format(message.toString(), args);
+        System.out.printf("%s [S4J] %s [%s] - %s%n", FORMAT.format(new Date()), level, Thread.currentThread().getName(),
                 message);
         if (t != null)
             t.printStackTrace();
+    }
+
+    private enum Level {
+        DEBUG, INFO, WARN, ERROR
     }
 }
